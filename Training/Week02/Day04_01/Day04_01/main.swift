3331168f95e7bd0b435b28c62a9c5dd826c1011a
shinseungjae <mok05247@naver.com>
2025-02-06 13:10:07 +0900

    Day05 업로드

--- Training/Week02/Day04_01/Day04_01/main.swift
+++ Training/Week02/Day04_01/Day04_01/main.swift
@@ -1,62 +1,29 @@
-// 알고리즘 공부,,
-// 입출력 예시
-// user_id:   ["frodo", "fradi", "crodo", "abc123", "frodoc"]
-// banned_id: ["fr*d*", "abc1**"]
-
-// ---------> result: 2
-
 import Foundation
 
-func solution(_ user_id:[String], _ banned_id:[String]) -> Int {
+func solution(_ s:String) -> Int {
+  let arr = Array(s)
   
-  var dict: [Int: [String]] = [:]
-  
-  for i in 0..<banned_id.count {
-    dict[i] = []
-    for j in 0..<user_id.count {
-      if isMatchingPattern(text: user_id[j], pattern: banned_id[i]) {
-        dict[i]?.append(user_id[j])
-      }
-    }
-  }
-  
-  print(dict)
   
   return 0
 }
 
-func isMatchingPattern(text: String, pattern: String) -> Bool {
-  let textArr = Array(text)
-  let patternArr = Array(pattern)
-  
-  guard textArr.count == patternArr.count else { return false }
-  
-  for i in 0..<textArr.count {
-    if patternArr[i] == "*" { continue }
-    if textArr[i] != patternArr[i] { return false }
-  }
-  
-  return true
-}
+solution("aabbaccc")
 
-//func DFS(_ graph: [[String]]) {
-//  var visitedQueue: [String] = []
-//  var needVisitStack: [String] = [graph[0][0]]
-//  
-//  while needVisitStack.isEmpty {
-//    
-//  }
-//  
-//}
+// "aabbaccc"  7
+// 1개 단위로 자른다 a/a/b/b/ ...
+// 2개 단위로 자른다 aa/bb/ac/cc
+// 3개 단위로 자른다 aab/bac/cc
+// 4개 단위로 자른다 aabb/accc
+// 5개 단위로 자른다 -> 불가
 
-let resul = solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"])
-print(resul)
+// "ababcdcdababcdcd"  9
+// 2 -> 2ab2cd2ab2cd
+// 3 -> aba, bab, bcd, cd
 
-// *rodo -> frodo, crodo
-// *rodo -> frodo, crodo
-// ****** -> abc123, frodoc
 
-// frodo, corodo, abc123
-// frodo, crodo, forodoc
-// DFS
+// "abcabcdede"  8
+// a, b, c
+// ab, bc, ca, ab,
 
+// "abcabcabcabcdededededede"  14
+// "xababcdcdababcdcd"  17
---
+++ Training/Week02/Day06/Day06/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  Day06
+//
+//  Created by 신승재 on 2/6/25.
+//
+
+import Foundation
+
+// 객체 지향 개념을 활용하여 "도형 그리기 프로그램을 구현하시오"
+class Shape {
+  func draw() {
+    print("도형을 그립니다.")
+  }
+}
+
+class Circle: Shape {
+  override func draw() {
+    print("원을 그립니다.")
+  }
+}
+
+class Rectangle: Shape {
+  override func draw() {
+    print("사각형을 그립니다.")
+  }
+}
+
+let circle = Circle()
+let rectangle = Rectangle()
+
+circle.draw()
+rectangle.draw()
