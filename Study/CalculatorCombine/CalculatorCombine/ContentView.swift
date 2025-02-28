5f7ddf713aea2ed01d39611cd873a4cf2ed88885
shinseungjae <mok05247@naver.com>
2025-02-28 11:01:35 +0900

    기능 추가 및 readme 업데이트

--- Study/CalculatorCombine/CalculatorCombine/ContentView.swift
+++ Study/CalculatorCombine/CalculatorCombine/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
           
           Spacer()
           
-          Text("\(viewModel.state.resultNumber)")
+          Text(viewModel.state.resultNumber)
             .font(.system(size: 70, weight: .medium))
             .foregroundStyle(.gray05)
             .padding(.trailing, 10)
@@ -38,62 +38,99 @@ struct ContentView: View {
           
           HStack(spacing: 0) {
             Button("AC", action: {
-              
+              viewModel.send(.allClear)
             })
               .buttonStyle(CalculatorButton(type: .special))
-            Button("+/-", action: {})
+            Button("+/-", action: {
+              viewModel.send(.operation(.toggle))
+            })
               .buttonStyle(CalculatorButton(type: .special))
-            Button("%", action: {})
+            Button("%", action: {
+              viewModel.send(.operation(.percent))
+            })
               .buttonStyle(CalculatorButton(type: .special))
-            Button("÷", action: {})
+            Button("÷", action: {
+              viewModel.send(.operation(.divide))
+            })
               .buttonStyle(CalculatorButton(type: .operation))
           }
           
           HStack(spacing: 0) {
-            Button("7", action: {})
+            Button("7", action: {
+              viewModel.send(.number("7"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("8", action: {})
+            Button("8", action: {
+              viewModel.send(.number("8"))
+              
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("9", action: {})
+            Button("9", action: {
+              viewModel.send(.number("9"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("x", action: {})
+            Button("×", action: {
+              viewModel.send(.operation(.multiply))
+            })
               .buttonStyle(CalculatorButton(type: .operation))
           }
           
           HStack(spacing: 0) {
-            Button("4", action: {})
+            Button("4", action: {
+              viewModel.send(.number("4"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("5", action: {})
+            Button("5", action: {
+              viewModel.send(.number("5"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("6", action: {})
+            Button("6", action: {
+              viewModel.send(.number("6"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("-", action: {})
+            Button("-", action: {
+              viewModel.send(.operation(.subtract))
+            })
               .buttonStyle(CalculatorButton(type: .operation))
           }
           
           HStack(spacing: 0) {
-            Button("1", action: {})
+            Button("1", action: {
+              viewModel.send(.number("1"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("2", action: {})
+            Button("2", action: {
+              viewModel.send(.number("2"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("3", action: {})
+            Button("3", action: {
+              viewModel.send(.number("3"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button("+", action: {})
+            Button("+", action: {
+              viewModel.send(.operation(.add))
+            })
               .buttonStyle(CalculatorButton(type: .operation))
           }
           
           HStack(spacing: 0) {
-            Button("0", action: {})
+            Button("0", action: {
+              viewModel.send(.number("0"))
+            })
               .buttonStyle(CalculatorButton(type: .number))
-            Button(".", action: {})
+            Button(".", action: {
+              viewModel.send(.dot)
+            })
               .buttonStyle(CalculatorButton(type: .number))
             Button {
-              print("action")
+              viewModel.send(.delete)
             } label: {
               Image(systemName: "delete.left")
                 .font(.system(size: 28, weight: .light))
             }.buttonStyle(CalculatorButton(type: .number))
-            Button("=", action: {})
+            Button("=", action: {
+              viewModel.send(.equal)
+            })
               .buttonStyle(CalculatorButton(type: .delete))
           }
         }.padding(25)
--- Study/CalculatorCombine/CalculatorCombine/ViewModel.swift
+++ Study/CalculatorCombine/CalculatorCombine/ViewModel.swift
@@ -11,21 +11,101 @@ import Foundation
 final class ViewModel {
   
   struct State {
-    var resultNumber = 0
+    var resultNumber: String = "0"
     var expressionText = ""
   }
   
   private(set) var state: State = .init()
-
+  private var recentInput = ""
   
   enum Action {
-    
+    case number(String)
+    case dot
+    case operation(OperationType)
+    case switchOperator
+    case delete
+    case allClear
+    case equal
   }
   
   func send(_ action: Action) {
     switch action {
+    case .number(let number):
+      handleNumberInput(number)
       
+    case .operation(let type):
+      handleOperationInput(type)
+      
+    case .switchOperator:
+      print("switch")
+      
+    case .delete:
+      if !state.expressionText.isEmpty {
+        state.expressionText.removeLast()
+        recentInput = state.expressionText.last.map { String($0) } ?? ""
+      }
+      
+    case .dot:
+      print("dot")
+      
+    case .allClear:
+      state.expressionText = ""
+      state.resultNumber = "0"
+      recentInput = ""
+      
+    case .equal:
+      calculateExpression()
     }
   }
   
+  private func handleNumberInput(_ number: String) {
+    if state.expressionText == "0" {
+      state.expressionText = number
+    } else {
+      state.expressionText += number
+    }
+    recentInput = number
+  }
+  
+  
+  private func handleOperationInput(_ type: OperationType) {
+    if recentInput.isEmpty || isOperator(recentInput) {
+      return
+    }
+    
+    state.expressionText += type.rawValue
+    recentInput = type.rawValue
+  }
+  
+  private func calculateExpression() {
+    if state.expressionText.isEmpty || isOperator(recentInput) {
+      return
+    }
+    
+    let modifiedExpression = state.expressionText
+      .replacingOccurrences(of: "(?<=\\d)(?=\\D)", with: ".0", options: .regularExpression)
+    
+    let expression = NSExpression(format: modifiedExpression)
+    
+    if let result = expression.expressionValue(with: nil, context: nil) as? NSNumber {
+      state.resultNumber = String(format: "%.2f", result.doubleValue)
+    } else {
+      state.resultNumber = "0"
+    }
+  }
+  
+  private func isOperator(_ text: String) -> Bool {
+    return ["+", "-", "*", "/"].contains(text)
+  }
 }
+
+// 연산자 타입 정의
+enum OperationType: String {
+  case add = "+"
+  case subtract = "-"
+  case multiply = "*"
+  case divide = "/"
+  case percent = "1"
+  case toggle = "2"
+}
+
