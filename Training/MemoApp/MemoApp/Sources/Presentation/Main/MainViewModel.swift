854e546087a45acdadc37212982219d713969097
shinseungjae <mok05247@naver.com>
2025-03-10 22:54:35 +0900

    ViewModel 수정

--- Training/MemoApp/MemoApp/Sources/Presentation/Main/MainViewModel.swift
+++ Training/MemoApp/MemoApp/Sources/Presentation/Main/MainViewModel.swift
@@ -8,7 +8,11 @@
 import Foundation
 import Combine
 
+
 final class MainViewModel {
+  
+  private var cancellables = Set<AnyCancellable>()
+  
   // View 로부터 들어오는 Input
   enum Input {
     case onAppear
@@ -17,11 +21,30 @@ final class MainViewModel {
   }
   
   // View로 나가는 출력들
+  private let output: PassthroughSubject<Output, Never> = .init()
   enum Output {
     case loadMemoDatas([Memo])
     case pushToDetailView
   }
   
-  // 구독한 이벤트를 모아놓는 변수
-  private var cancellables = Set<AnyCancellable>()
+  // UI에 직접적으로 연관이 되는 변수(상태를 저장하고 변경 사항을 자동으로 방출하는데 사용됨, 예를 들어, textField)
+  @Published var viewType: ViewType = .table
+  @Published var memos: [Memo] = []
+  
+  func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+    input.receive(on: DispatchQueue.main)
+      .sink { [weak self] event in
+        switch event {
+        case .onAppear:
+          print(#function, "OnAppear")
+        case .addButtonTapped:
+          print(#function, "addButtonTapped")
+        case .cellTapped(let index):
+          print(#function, "cellTapped")
+        }
+      }
+      .store(in: &cancellables)
+    
+    return output.eraseToAnyPublisher()
+  }
 }
